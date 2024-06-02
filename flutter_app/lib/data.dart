226f9865f35ef4abe7861bac3857725565111a88
jamilah <nantjami@gmail.com>
2024-06-02 09:45:08 +0300

    Learning module

---
+++ flutter_app/lib/data.dart
@@ -0,0 +1,43 @@
+//small json data assigned to variable data to be used in FlashcardScreen widget
+var data = {
+  "Alphabet": [
+    {"image": "assets/images/A.jpg", "letter": "A"},
+    {"image": "assets/images/B.jpg", "letter": "B"},
+    {"image": "assets/images/C.jpg", "letter": "C"},
+    {"image": "assets/images/E.jpg", "letter": "E"},
+    {"image": "assets/images/F.jpg", "letter": "F"},
+    {"image": "assets/images/G.jpg", "letter": "G"},
+    {"image": "assets/images/H.jpg", "letter": "H"},
+    {"image": "assets/images/I.jpg", "letter": "I"},
+    {"image": "assets/images/J.jpg", "letter": "J"},
+    {"image": "assets/images/K.jpg", "letter": "K"},
+    {"image": "assets/images/L.jpg", "letter": "L"},
+    {"image": "assets/images/M.jpg", "letter": "M"},
+    {"image": "assets/images/N.jpg", "letter": "N"},
+    {"image": "assets/images/O.jpg", "letter": "O"},
+    {"image": "assets/images/P.jpg", "letter": "P"},
+    {"image": "assets/images/Q.jpg", "letter": "Q"},
+    {"image": "assets/images/R.jpg", "letter": "R"},
+    {"image": "assets/images/S.jpg", "letter": "S"},
+    {"image": "assets/images/T.jpg", "letter": "T"},
+    {"image": "assets/images/U.jpg", "letter": "U"},
+    {"image": "assets/images/V.jpg", "letter": "V"},
+    {"image": "assets/images/W.jpg", "letter": "W"},
+    {"image": "assets/images/X.jpg", "letter": "X"},
+    {"image": "assets/images/Y.jpg", "letter": "Y"},
+    {"image": "assets/images/Z.jpg", "letter": "Z"}
+  ],
+  "Numbers": [
+    {"image": "assets/images/0.jpeg", "letter": "0"},
+    {"image": "assets/images/1.jpeg", "letter": "1"},
+    {"image": "assets/images/2.jpeg", "letter": "2"},
+    {"image": "assets/images/3.jpeg", "letter": "3"},
+    {"image": "assets/images/4.jpeg", "letter": "4"},
+    {"image": "assets/images/5.jpeg", "letter": "5"},
+    {"image": "assets/images/6.jpeg", "letter": "6"},
+    {"image": "assets/images/7.jpeg", "letter": "7"},
+    {"image": "assets/images/8.jpeg", "letter": "8"},
+    {"image": "assets/images/9.png", "letter": "9"},
+    {"image": "assets/images/10.jpeg", "letter": "10"}
+  ],
+};
